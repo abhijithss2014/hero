d8afb3c6047ee0a90fee52b53cc445f9cf46056a
abhijithss2010 <abhijithss2010@gmail.com>
2017-05-20 22:03:38 +0530

    hero component start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+// import { HeroComponent } from './hero/hero.component';
+
+
+export class Hero {
+  id: number;
+  name: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'app works!';
+  title = 'Hero ';
+  hero: Hero = {
+    id: 20,
+    name: 'abhijith'
+    
+  };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { HeroComponent } from './hero/hero.component';
+// import { HeroComponent } from './hero/hero.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeroComponent
+    AppComponent
+    // HeroComponent
   ],
   imports: [
     BrowserModule,
